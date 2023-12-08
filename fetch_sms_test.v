2ae0c164c6f713eb5f68f431f020e6f122f82562
smichea <smichea@gmail.com>
2023-12-08 02:19:45 +0100

    apply v formatting

--- fetch_sms_test.v
+++ fetch_sms_test.v
@@ -1,22 +1,20 @@
 module twilio
-//sid="SM854574a774a7519546c12f5c11ccf549"
-
 
 fn test_fetch_sms_success() {
-    // Setup
-    client := TwilioClient{
-        account_sid: 'ACd6******************************',
-        auth_token: '76******************************',
-        from_number: '+12345678901',
-    }
+	// Setup
+	client := TwilioClient{
+		account_sid: 'ACd6******************************'
+		auth_token: '76******************************'
+		from_number: '+12345678901'
+	}
 
-    // Execute
-    result := client.fetch_sms('SMa6******************************') or {
-		eprintln(err.msg())	
-        assert false // Test should not fail here
-        return
-    }
+	// Execute
+	result := client.fetch_sms('SMa6******************************') or {
+		eprintln(err.msg())
+		assert false // Test should not fail here
+		return
+	}
 
-    // Assert
-    assert result.status == 'delivered'
+	// Assert
+	assert result.status == 'delivered'
 }
